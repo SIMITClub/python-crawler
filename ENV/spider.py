65845e7291af67cd11dba3e215b0370f1cc0d979
Kenneth Chew <kence@Kenneths-MacBook-Pro.local>
2017-09-16 20:14:21 +0800

    Basic Functionality Up and Running

--- ENV/spider.py
+++ ENV/spider.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-import re
+
 import requests
-import json
 from bs4 import BeautifulSoup
 import os
-import math
-# TEST
+import sys
+
+
 def print_greeting():
     print("Crawler running....")
     # Replace any spaces in the directory name with underscores
@@ -72,9 +72,7 @@ def query_google(dir_name,num,params):
         with open(file_name, "wb") as file:
             file.write(soup.prettify().encode("utf-8"))
 
-
-    print("Saving results.....")
-    print("Results saved.\n\n")
+    print("Saving results.....\n Results saved.\n\n")
     return file_names
 
 
@@ -96,12 +94,80 @@ def get_search_links(search_results):
         for each in links:
             f.write(each + "\n")
         print("Links saved.")
+    return links
 
 
+def crawl_links(set_of_links):
+
+    # Create results dir
+    results_dir = dir_name + "/results"
+    if not os.path.exists(results_dir):
+        print("Creating directory to store results")
+        os.makedirs(results_dir)
+        print("Directory created.")
+    else:
+        print("Directory already exists, continuing with web scraping")
+
+    user_agent = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"}
+
+    results_set = set()
+
+    for each in set_of_links:
+        print("Crawling through link...")
+        r = requests.get(each, headers=user_agent, timeout=10.0)
+        html = r.text.encode("utf-8")
+        soup = BeautifulSoup(html,"html.parser")
+        body = soup.body.encode("utf-8")
+
+        # Get file name(E.g google from https://ww.google.com
+        print("Original: " + str(each))
+        splitted = each.split(".")
+        # E.g http://www.abc.com/example/year/month/day/page.html -> length of 4
+        # splitted[-3] => abc
+        if(len(splitted) > 3):
+            file_name = splitted[-3]
+        else:
+            # E.g http://abc.com OR http://www.abc.com
+            # splitted[-2] => abc
+            file_name = splitted[-2]
+        if "http" in file_name:
+            file_name = file_name.split("//")[1]
+        print("Domain Name: " + str(file_name))
+        results_set.add((file_name,body))
+
+    # Used to check duplicate file names arising from multiple links from the same domain name
+    file_counter = 0
+
+    # Save results to file
+    for tup in results_set:
+
+        # Check if name is too long(Indicative of error in getting file name above)
+        if len(tup[0]) > 20:
+            name = tup[0][:20]
+        else:
+            name = tup[0]
+
+        # Replaces any / in the name with _ if they exists
+        if "/" in name:
+            name = name.replace("/", "_")
+
+        # Check if file name already exists(Multiple pages from same site)
+        if os.path.exists(results_dir + "/" + name + ".html"):
+            file_counter += 1
+            save_name = results_dir + "/" + name + str(file_counter) + ".html"
+
+        else:
+            file_counter = 0
+            save_name = results_dir + "/" + name + ".html"
+
+        print("Saving file: " + name)
+        with open(save_name,"w") as f:
+            f.write(str(tup[1]))
 
 
 
 if __name__ == "__main__":
     dir_name,num,params = print_greeting()
     query_results = query_google(dir_name,num,params)
-    get_search_links(query_results)
+    search_links = get_search_links(query_results)
+    crawl_links(search_links)
